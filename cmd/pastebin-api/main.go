34b4fa283dbcb3583cc6c6d801ff8a9616d2bdc6
kasyap1234 <kasyap3103@gmail.com>
2025-12-19 13:53:14 +0530

    add pastebin

--- cmd/pastebin-api/main.go
+++ cmd/pastebin-api/main.go
@@ -31,9 +31,25 @@ import (
 )
 
 func main() {
-	err := godotenv.Load("../../.env")
-	if err != nil {
-		log.Printf("Warning: .env file not found, using system env vars: %v", err)
+	// Try to load .env file from multiple possible locations
+	envPaths := []string{
+		".env",       // Current directory
+		"../../.env", // From cmd/pastebin-api/ relative to project root
+		"../.env",    // From bin/ directory
+		"./.env",     // Explicit current directory
+	}
+
+	envLoaded := false
+	for _, path := range envPaths {
+		if err := godotenv.Load(path); err == nil {
+			log.Printf("Loaded .env file from: %s", path)
+			envLoaded = true
+			break
+		}
+	}
+
+	if !envLoaded {
+		log.Printf("Warning: .env file not found in any of the expected locations, using system env vars")
 	}
 
 	application, appErr := app.New()
